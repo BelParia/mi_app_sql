1a10f48eca2376785c0c35d134cda9e71ee3262d
Roly <73922607+BelParia@users.noreply.github.com>
2026-08-13 17:27:53 -0500

    Arreglar render de la vista index
    Corrige el typo en el nombre del template y el import de render_template.

--- routes.py
+++ routes.py
@@ -1,4 +1,12 @@
+from flask import render_template
+
 def register_app(app):    
     @app.route("/")
     def inicio():
-        return "Hola mundo"
+        titulo = "Mi aplicación web"
+        elementos = [
+            {},
+            {},
+            {}]
+        return render_template("index.html", titulo = titulo, elementos = elementos)
+    
